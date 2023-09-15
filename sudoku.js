7ca7be1b501d6d13d20f6173f71e43423f3730de
Sergei Komarov <komarovs87@mail.ru>
2023-09-15 11:56:52 +0300

    read2

--- sudoku.js
+++ sudoku.js
@@ -1,18 +1,26 @@
 function read() {
   const { EOL } = require('os');
   const fs = require('fs');
+  const some = process.argv;
   const path = require('path');
   const dirName = __dirname;
   const dirPath = path.join(`${dirName}/puzzles.txt`);
-  const txt = fs.readFileSync(dirPath, 'utf-8');
-  return txt
+  let txt = fs.readFileSync(dirPath, 'utf-8');
+
+  txt = txt
     .trim()
     .split(EOL)
-    .map((el) => Array.from(el));
+    .map((el) => Array.from(el))[some[2]];
+  let subArray = [];
+  for (let i = 0; i < txt.length / 9; i += 1) {
+    subArray[i] = txt.slice(i * 9, i * 9 + 9);
+  }
+  return subArray;
 
   //  * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
   //  */
 }
+
 const puzzles = read();
 console.log(puzzles);
 function solve() {
@@ -30,9 +38,7 @@ function isSolved() {
 }
 
 function prettyBoard() {
-  for (let i=0;i<puzzles.length;i++)
-const row = 
-
+  for (let i = 0; i < puzzles.length; i++) {}
 
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
